21e8e24f3ee03134c890b3e3c0001f22a799a82f
Osama Hajj Ali <ossama.nae12@gmail.com>
2021-10-09 15:05:08 +0300

    commented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
-const TEXT = "Hello, I'm Osama and I develop web applications!";
+const TEXT = "Hello there, I'm Osama and I develop web apps!";
 const TEXT_SIZE = TEXT.length;
 
 function App() {
@@ -9,30 +9,23 @@ function App() {
   const [textIndex, setTextIndex] = useState(0);
   const [text, setText] = useState('');
 
-  useEffect(() => {
-    setTimeout(typeText, 100);
-  })
+  useEffect(() => setTimeout(typeText, 100))
   
+  //Called onChange event of the text div, types text until text index matches text size
   function typeText(){
     if (textIndex === TEXT_SIZE + 1) return;
     else {
-      if (textIndex === TEXT_SIZE) 
-        setText(TEXT.substring(0, textIndex));
-      else 
-        setText(TEXT.substring(0, textIndex) + "_");
-
+      setText(TEXT.substring(0, textIndex));
       setTextIndex(textIndex + 1);
     }
   }
 
+  //Checks if index of text is equal to text size, used to apply toggleCursor animation when typing animation is done
   const typingCompleted = () => textIndex === TEXT_SIZE + 1;
 
   return (
     <div className="App">
-      <div onChange={typeText}>
-        {text}
-        <span>{typingCompleted() ? '_' : ''}</span>
-      </div>
+      <div onChange={typeText}>{text}<span style={{ animationName: typingCompleted() ? "toggleCursor" : "" }}>_</span></div>
     </div>
   );
 }
